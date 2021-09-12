6d760982ad95a166847b5fd5d074adfe0435dbf2
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2021-09-12 22:45:40 +0530

    001: question 2 solved

---
+++ 001_operators_branching_loops/intermediate/question2/main.js
@@ -0,0 +1,16 @@
+// Print the following star pattern :-
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+let storeStar = "*"
+for(let i =0;i<5;i++){
+    console.log(storeStar)
+        storeStar = storeStar + "*"
+    
+//  console.log("\n")
+
+}
